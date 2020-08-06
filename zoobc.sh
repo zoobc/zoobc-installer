d33e331e2d8558d1e0b63692b5bf722c2002e596
astaphobia <ariasta.ketut@icloud.com>
2020-08-06 15:16:22 +0800

    save what have done for after demo

--- zoobc.sh
+++ zoobc.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+# ZOOBC INSTALLER SCRIPT
+# version: v1
+# Description: Installation script for zoobc node. A simple script allowing to download th binary and generating configuration file
+# ------------------------------------------------------------------------------------------------------------------------------
 zbc_dir=zoobc
+zbc_config=config.toml
 zbc_binary=zoobc
+
+peerPort=8001
+apiRPCPort=7000
+apiHTTPPort=7001
+smithing=true
+wellknownPeers=
+declare -a peersInput
+
+# join array by limiter
+function join_by { local IFS="$1"; shift; echo "$*"; }
 
 # get platform will check os for installation purposes
 function get_platform(){
@@ -44,15 +59,69 @@ function download_binary(){
 if [ ! -f ~/${zbc_dir}/${zbc_binary} ]; then
   echo "DOWNLOADING BINARY ..."
   # shellcheck disable=SC2046
-  cd ~/${zbc_dir} && curl -O http://172.104.47.168/$(get_platform)/zoobc
-  chmod 755 ~/${zbc_dir}/zoobc
+  cd ~/${zbc_dir} && curl -O http://172.104.47.168/$(get_platform)/$zbc_binary
+  chmod 755 ~/${zbc_dir}/zbc_binary
 fi
 }
+
+# checking the depends files like configuration files and more stuff
+function checking_depends(){
+if [ ! -f ~/${zbc_dir}/${zbc_config} ]; then
+  read -p 'PEER PORT: ' peerPort
+  if [[ -n ${peerPort//[0-9]/} ]]; then
+    echo "Invalid input, must be a number"
+    exit 2
+  fi
+  read -p 'API RPC PORT: ' apiRPCPort
+  if [[ -n ${peerPort//[0-9]/} ]]; then
+    echo "Invalid input, must be a number"
+    exit 2
+  fi
+  read -p 'API HTTP PORT: ' apiHTTPPort
+  if [[ -n ${peerPort//[0-9]/} ]]; then
+    echo "Invalid input, must be a number"
+    exit 2
+  fi
+  read -p 'SMITHING, TRUE|FALSE? '  smithing
+  read -p 'WELLKNOWN PEERS, SEPARATED BY SPACE: ' peersInput
+  echo
+
+  # separating peers input
+  wellknownPeers=$(join_by , $peersInput)
+
+  # shellcheck disable=SC1044
+  cat > ${zbc_config} <<EOF
+resourcePath="./resource"
+dbName="zoobc.db"
+badgerDBName="zoobc.kv/"
+nodeKeyFile="node_keys.json"
+snapshotPath="./resource/snapshots"
+
+peerPort=$peerPort
+apiRPCPort=$apiRPCPort
+apiHTTPPort=$apiHTTPPort
+monitoringPort=9090
+cpuProfilingPort=6060
+
+maxAPIRequestPerSecond=10
+apiReqTimeoutSec=2
+smithing=$smithing
+proofOfOwnershipReqTimeoutSec=2
+
+wellknownPeers=[$wellknownPeers]
+ownerAccountAddress=""
+logLevels=["panic"]
+EOF
+
+cp $zbc_config ~/$zbc_dir/$zbc_config
+fi
+}
+
 
 if [ ! -d ~/$zbc_dir ]; then
   mkdir ~/$zbc_dir
 fi
 if install_curl; download_binary; then
-  cd ~/${zbc_dir} && ./zoobc
+  cd ~/${zbc_dir} && ./$zbc_binary
 #	printf "Finish\nNow run the app"
 fi
