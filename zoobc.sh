94337b33eb9a0c3463b497956d22e939134bec63
astaphobia <ariasta.ketut@icloud.com>
2020-08-06 14:49:20 +0800

    some enhancement, check directory and more

--- zoobc.sh
+++ zoobc.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-dir=zoobc
-# platform will check os for installation purposes
-function platform(){
+zbc_dir=zoobc
+zbc_binary=zoobc
+
+# get platform will check os for installation purposes
+function get_platform(){
 os="$(uname)"
 case $os in
 'Darwin')
@@ -26,28 +28,31 @@ echo "$os"
 # check and install curl
 function install_curl(){
 if ! curl --version &> /dev/null; then
-	echo "DOWNLOADING CURL"
-	case $(platform) in
-		'mac')
-			brew install curl;;
-		'linux')
-			apt install curl -y;;
-		*);;
-	esac
+  echo "DOWNLOADING CURL ..."
+  case $(get_platform) in
+    'mac')
+      brew install curl;;
+    'linux')
+      apt install curl -y;;
+    *);;
+  esac
 fi
 }
 
 # downloading binary file from host
 function download_binary(){
-	echo "DOWNLOADING BINARY"
-	# shellcheck disable=SC2088
-	mkdir ~/${dir}
-	# shellcheck disable=SC2046
-	cd ~/${dir} && curl -O http://172.104.47.168/$(platform)/zoobc
-	chmod 755 ~/${dir}/zoobc
+if [ ! -f ~/${zbc_dir}/${zbc_binary} ]; then
+  echo "DOWNLOADING BINARY ..."
+  # shellcheck disable=SC2046
+  cd ~/${zbc_dir} && curl -O http://172.104.47.168/$(get_platform)/zoobc
+  chmod 755 ~/${zbc_dir}/zoobc
+fi
 }
 
+if [ ! -d ~/$zbc_dir ]; then
+  mkdir ~/$zbc_dir
+fi
 if install_curl; download_binary; then
-  cd ~/${dir} && ./zoobc
+  cd ~/${zbc_dir} && ./zoobc
 #	printf "Finish\nNow run the app"
 fi
