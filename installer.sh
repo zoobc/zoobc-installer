4c2b9c4af0d5f0bc9ce4436ce1af0aa6cbfafa40
Ariasa Ketut <ariasta.ketut@icloud.com>
2020-08-25 16:00:16 +0800

    Script include daemon service (#4)
    * script with daemon service generator
    * make sure service working well via launchd

--- installer.sh
+++ installer.sh
@@ -7,6 +7,7 @@ zbc_resource=resource
 wallet_cert=wallet.zbc
 target=$1
 
+# get_platform just inform what kind os version
 function get_platform() {
   os="$(uname)"
   case $os in
@@ -33,7 +34,7 @@ function get_platform() {
   echo "$os"
 }
 
-# checking curl
+# install_curl checking curl
 function install_curl() {
   if ! curl --version &>/dev/null; then
     echo 'DOWNLOADING CURL ...'
@@ -48,7 +49,7 @@ function install_curl() {
   fi
 }
 
-# downloading binary file from host
+# download_binary downloading binary file from host
 function download_binary() {
   if [ ! -f ~/${zbc_dir}/${zbc_binary} ]; then
     echo "DOWNLOADING ZOOBC BINARY ..."
@@ -64,16 +65,103 @@ function download_binary() {
   fi
 }
 
+# generate_service will generate service script to systemd called zoobc.service
+function generate_service() {
+  echo 'Generating daemon service ...'
+  case $(get_platform) in
+  'linux')
+    if [ ! -f "/lib/systemd/system/zoobc.service" ]; then
+      cat >zoobc.service <<EOF
+[Unit]
+Description=zoobc node service
+[Service]
+Type=simple
+User=root
+Group=root
+#emergency (0), alert (1), critical (2), error (3), warning (4), notice (5), info (6), and debug (6)
+LogLevelMax=3
+WorkingDirectory=~/zoobc
+ExecStart=~/zoobc/zoobc --debug --cpu-profile
+Restart=on-failure
+RestartSec=3
+
+[Install]
+WantedBy=multi-user.target
+EOF
+      sudo cp zoobc.service /lib/systemd/system/zoobc.service
+      systemctl daemon-reload
+    fi
+    ;;
+  'darwin')
+    if [ ! -f /Library/LaunchDaemons/org.zoobc.startup.plist ]; then
+      cat >org.zoobc.startup.plist <<EOF
+<?xml version"1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+  <key>Label</key>
+  <string>org.zoobc.node</string>
+  <key>ServiceDescription</key>
+  <string>ZOOBC NODE Service zoobc.com</string>
+  <key>WorkingDirectory</key>
+  <string>$HOME/zoobc</string>
+  <key>ProgramArguments</key>
+  <array>
+    <string>./zoobc</string>
+  </array>
+  <key>UserName</key>
+  <string>root</string>
+  <key>GroupName</key>
+  <string>root</string>
+  <key>RunAtLoad</key>
+  <true/>
+  <key>KeepAlive</key>
+  <true/>
+</dict>
+</plist>
+EOF
+      sudo cp org.zoobc.startup.plist /Library/LaunchDaemons
+      launchctl load /Library/LaunchDaemons/org.zoobc.startup.plist
+    fi
+    ;;
+  *)
+    echo "For $(get_platform) please check on zoobc.com for more detail"
+    ;;
+  esac
+}
+
+function start_service() {
+  echo "Starting Service ..."
+  case $(get_platform) in
+  'darwin')
+    launchctl load /Library/LaunchDaemons/org.zoobc.startup.plist
+    ;;
+  'linux')
+    service zoobc start
+    ;;
+  *)
+    echo "For $(get_platform) please check on zoobc.com for more detail"
+    ;;
+  esac
+}
+
+################
+# MAIN PROCESS #
+################
 if [[ $target =~ dev|staging|alpha|beta ]]; then
   # checking zoobc directory
-  [ -f "./${wallet_cert}" ] && cp wallet.zbc ~/${zbc_dir}/${wallet_cert}
   [ ! -d ~/$zbc_dir ] && mkdir ~/$zbc_dir
-  [ ! -d ~/$zbc_dir/$zbc_resource ] && mkdir ~/$zbc_dir/$zbc_resource 
+  # checking resource directory
+  [ ! -d ~/$zbc_dir/$zbc_resource ] && mkdir ~/$zbc_dir/$zbc_resource
+  # copying existsing certificate file
+  [ -f $wallet_cert ] && cp $wallet_cert ~/${zbc_dir}/${wallet_cert}
   if
     install_curl
     download_binary
   then
     cd ~/${zbc_dir} && ./${zbc_cmd_binary} configure -t="$target"
+    generate_service
+    start_service
   fi
 else
   echo 'usage: sh ./installer.sh dev|staging|alpha'
