27d069f2579720359f959a170c193a39b4395bb8
astaphobia <ariasta.ketut@icloud.com>
2020-08-26 14:53:08 +0800

    zoobc directory in specific target

--- installer.sh
+++ installer.sh
@@ -49,16 +49,20 @@ function install_curl() {
   fi
 }
 
+# get_dir_target to get joined target directory
+function get_dir_target(){
+  echo "$HOME/$zbc_dir.$target"
+}
 # download_binary downloading binary file from host
 function download_binary() {
   echo "DOWNLOADING ZOOBC BINARY ..."
   # shellcheck disable=SC2046
-  cd $HOME/${zbc_dir} && curl -O http://172.104.47.168/$target/$(get_platform)/$zbc_binary
-  chmod 755 $HOME/${zbc_dir}/${zbc_binary}
+  cd $(get_dir_target) && curl -O http://172.104.47.168/"$target"/$(get_platform)/$zbc_binary
+  chmod 755 "$(get_dir_target)/$zbc_binary"
   echo "DOWNLOADING ZOOBC CMD BINARY ..."
   # shellcheck disable=SC2046
-  cd $HOME/${zbc_dir} && curl -O http://172.104.47.168/$target/$(get_platform)/$zbc_cmd_binary
-  chmod 755 $HOME/${zbc_dir}/${zbc_cmd_binary}
+  cd $(get_dir_target) && curl -O http://172.104.47.168/"$target"/$(get_platform)/$zbc_cmd_binary
+  chmod 755 "$(get_dir_target)/$zbc_cmd_binary"
 }
 
 # generate_service will generate service script to systemd called zoobc.service
@@ -76,7 +80,7 @@ User=root
 Group=root
 #emergency (0), alert (1), critical (2), error (3), warning (4), notice (5), info (6), and debug (6)
 LogLevelMax=3
-WorkingDirectory=$HOME/zoobc
+WorkingDirectory=$HOME/$(get_dir_target)
 ExecStart=$HOME/zoobc/zoobc --debug --cpu-profile
 Restart=on-failure
 RestartSec=3
@@ -100,7 +104,7 @@ EOF
   <key>ServiceDescription</key>
   <string>ZOOBC NODE Service zoobc.com</string>
   <key>WorkingDirectory</key>
-  <string>$HOME/zoobc</string>
+  <string>$HOME/$(get_dir_target)</string>
   <key>ProgramArguments</key>
   <array>
     <string>./zoobc</string>
@@ -145,16 +149,16 @@ function start_service() {
 ################
 if [[ $target =~ dev|staging|alpha|beta ]]; then
   # checking zoobc directory
-  [ ! -d $HOME/$zbc_dir ] && mkdir $HOME/$zbc_dir
+  [ ! -d "$(get_dir_target)" ] && mkdir "$(get_dir_target)"
   # checking resource directory
-  [ ! -d $HOME/$zbc_dir/$zbc_resource ] && mkdir $HOME/$zbc_dir/$zbc_resource
-  # copying existsing certificate file
-  [ -f $wallet_cert ] && cp $wallet_cert $HOME/${zbc_dir}/${wallet_cert}
+  [ ! -d "$(get_dir_target)/$zbc_resource" ] && mkdir "$(get_dir_target)/$zbc_resource"
+  # copying existing certificate file
+  [ -f $wallet_cert ] && cp $wallet_cert "$(get_dir_target)/$wallet_cert"
   if
     install_curl
     download_binary
   then
-    cd $HOME/${zbc_dir} && ./${zbc_cmd_binary} configure -t="$target"
+    cd "$(get_dir_target)" && ./$zbc_cmd_binary configure -t="$target"
     generate_service
     echo "Installation finish."
     case $(get_platform) in
@@ -166,7 +170,7 @@ if [[ $target =~ dev|staging|alpha|beta ]]; then
       ;;
     *) ;;
     esac
-    echo "Or running manually: go to $HOME/{zbc_dir} and run ./${zbc_binary}"
+    echo "Or running manually: cd $(get_dir_target) && ./$zbc_binary"
   fi
 else
   echo 'usage: sh ./installer.sh dev|staging|alpha|beta'
